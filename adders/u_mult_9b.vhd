ea20891478911a001ce88000695e20a755344e7f
Nick Mascioli <namsc013@uottawa.ca>
2025-05-16 14:13:32 -0400

    Commented /adders/u_mult_9b.vhd

--- adders/u_mult_9b.vhd
+++ adders/u_mult_9b.vhd
@@ -1,112 +1,114 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
-ENTITY u_mult_test IS
-    PORT(
-        opA, opB : IN std_logic_vector(8 DOWNTO 0);
-        res      : OUT std_logic_vector(17 DOWNTO 0)
+entity u_mult_test is
+    port(
+        opA, opB : in std_logic_vector(8 downto 0);
+        res      : out std_logic_vector(17 downto 0)
     );
-END u_mult_test;
+end u_mult_test;
 
-ARCHITECTURE rtl OF u_mult_test IS
-    COMPONENT adder1bit
-        PORT(
-            a, b, cIn : IN std_logic;
-            sum, cOut : OUT std_logic
+architecture rtl of u_mult_test is
+    component adder1bit
+        port(
+            a, b, cIn : in std_logic;
+            sum, cOut : out std_logic
         );
-    END COMPONENT;
+    end component;
 
-    -- Use array of std_logic_vector instead of 2D array
-    TYPE slv_8 IS ARRAY(1 TO 8) OF std_logic_vector(1 TO 8);
-    TYPE slv_9 IS ARRAY(1 TO 8) OF std_logic_vector(0 TO 8);
+    type sum_arr is array(1 to 8) of std_logic_vector(1 to 8);
+    type cry_arr is array(1 to 8) of std_logic_vector(0 to 8);
 
-    SIGNAL s : slv_8 := (OTHERS => (OTHERS => '0'));
-    SIGNAL c : slv_9 := (OTHERS => (OTHERS => '0'));
+    signal s : sum_arr := (others => (others => '0'));
+    signal c : cry_arr := (others => (others => '0'));
 
-BEGIN
+begin
     -- LSB of result is the first partial product
-    res(0) <= opA(0) AND opB(0);
+    res(0) <= opA(0) and opB(0);
 
-    FIRST_COLUMN_GEN: FOR col IN 0 TO 8 GENERATE
-        FIRST_COL0_GEN: IF col = 0 GENERATE
-            adder0: adder1bit PORT MAP(
-                a    => opA(1) AND opB(0),
-                b    => opA(0) AND opB(1),
+    -- First row of adders
+    first_row: for col in 0 to 8 generate
+        first_row_col0: if col = 0 generate
+            adder0: adder1bit port map(
+                a    => opA(1) and opB(0),
+                b    => opA(0) and opB(1),
                 cIn  => '0',
                 sum  => res(1),
                 cOut => c(1)(0)
             );
-        END GENERATE;
+        end generate;
 
-        FIRST_COLN_GEN: IF col > 0 AND col < 8 GENERATE
-            addern: adder1bit PORT MAP(
-                a    => opA(col+1) AND opB(0),
+        first_row_colx: if col > 0 and col < 8 generate
+            addern: adder1bit port map(
+                a    => opA(col+1) and opB(0),
                 b    => opA(col) and opB(1),
                 cIn  => c(1)(col-1),
                 sum  => s(1)(col),
                 cOut => c(1)(col)
             );
-        END GENERATE;
+        end generate;
 
-        LAST_COLN_GEN: IF col = 8 GENERATE
-        addern: adder1bit PORT MAP(
-            a    => '0',
-            b    => opA(col) and opB(1),
-            cIn  => c(1)(col-1),
-            sum  => s(1)(col),
-            cOut => c(1)(col)
-        );
-        END GENERATE;
-    END GENERATE;
+        first_row_col8: if col = 8 generate
+            addern: adder1bit port map(
+                a    => '0',
+                b    => opA(col) and opB(1),
+                cIn  => c(1)(col-1),
+                sum  => s(1)(col),
+                cOut => c(1)(col)
+            );
+        end generate;
+    end generate;
 
     -- Main adder grid
-    GRID_GEN: FOR row IN 2 TO 8 GENERATE
-        COL_GEN: FOR col IN 0 TO 8 GENERATE
-            
-            GRID0: IF col = 0 GENERATE
-                adder0: adder1bit PORT MAP(
-                    a    => opA(0) AND opB(row),
+    grid_row: for row in 2 to 8 generate
+        grid_col: for col in 0 to 8 generate
+
+            col0: if col = 0 generate
+                adder0: adder1bit port map(
+                    a    => opA(0) and opB(row),
                     b    => s(row-1)(1),
                     cIn  => '0',
                     sum  => res(row),
                     cOut => c(row)(0)
                 );
-            END GENERATE;
+            end generate;
 
-            GRIDN: IF col > 0 AND col /= 8 GENERATE
-                addern: adder1bit PORT MAP(
-                    a    => opA(col) AND opB(row),
+            gridx: if col > 0 and col /= 8 generate
+                addern: adder1bit port map(
+                    a    => opA(col) and opB(row),
                     b    => s(row - 1)(col+1),
                     cIn  => c(row)(col - 1),
                     sum  => s(row)(col),
                     cOut => c(row)(col)
                 );
-            END GENERATE;
-   
-            END_CELL: IF col > 0 AND col = 8 GENERATE
-                addern: adder1bit PORT MAP(
-                    a    => opA(col) AND opB(row),
+            end generate;
+
+            -- Uses MSB carry from previous row
+            gridx_col8: if col > 0 and col = 8 generate
+                addern: adder1bit port map(
+                    a    => opA(col) and opB(row),
                     b    => c(row-1)(8),
                     cIn  => c(row)(col - 1),
                     sum  => s(row)(col),
                     cOut => c(row)(col)
                 );
-            END GENERATE;
+            end generate;
 
-        END GENERATE;
-    END GENERATE;
+        end generate;
+    end generate;
 
-    adder_half: adder1bit PORT MAP(
+    -- Final half adder with MSB
+    adder_half: adder1bit port map(
         a    => '0',
         b    => '0',
         cIn  => c(8)(8),
         sum  => res(17),
-        cOut => OPEN
+        cOut => open
     );
 
-    -- Final sums from s(7)(1 to 8)
-    FINAL_SUMS: FOR i IN 0 TO 7 GENERATE
+    -- Final sums from s(8)(1 to 8)
+    final_sums: for i in 0 to 7 generate
         res(9+i) <= s(8)(1+i);
-    END GENERATE;
+    end generate;
 
-END ARCHITECTURE;
+end architecture;
